070cc19ae75e070f56404af10296082770eed78c
arjun011 <arjunpatel011@hotmail.com>
2025-09-17 11:45:41 +0100

    removed ghost folder

---
+++ NiyasKitchenInventoryTests/Inventory/Mocks/InventoryListServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  MockInventoryListServices.swift
+//  NiyasKitchenInventoryTests
+//
+//  Created by Arjun on 16/09/25.
+//
+
+import Foundation
+@testable import NiyasKitchenInventory
+final class MockInventoryListServices: InventoryListServices {
+    
+}
